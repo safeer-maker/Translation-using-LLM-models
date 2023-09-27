23ba4da5767950d6c579129df4f4b5f91a25780e
Your Name <you@example.com>
2023-09-27 07:19:29 +0500

    Function to translate

--- Translating_with_model.py
+++ Translating_with_model.py
@@ -6,6 +6,14 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
 # Translate English to French
+
+def Translate_EN_to_FR (text):
+    input_ids = tokenizer(text, return_tensors="pt")
+    fr_token = model.generate(**input_ids).squeeze()
+    french = tokenizer.decode(fr_token, skip_special_tokens=True)
+    return french
+
+'''
 sample_text = "I am learning Natural Language Processing with Hugging Face, Lets Tanslate into french"
 
 input_ids = tokenizer(sample_text, return_tensors="pt")
@@ -15,3 +23,8 @@ fr_token = model.generate(**input_ids).squeeze()
 french = tokenizer.decode(fr_token, skip_special_tokens=True)
 
 print (french)
+'''
+
+result = Translate_EN_to_FR("I am learning Natural Language Processing with Hugging Face, Lets Tanslate into french")
+
+print (result)
